1a42ba4430c32b8968097976b1476772d587166d
Musafak <musafak.ft18@mail.umy.ac.id>
2020-06-29 10:23:33 +0700

    Deklarasi variable di class activity asmaul husna

--- app/src/main/java/com/example/asmaulhusna/ActivityAsmaulHusna.java
+++ app/src/main/java/com/example/asmaulhusna/ActivityAsmaulHusna.java
@@ -1,4 +1,16 @@
 package com.example.asmaulhusna;
 
-public class ActivityAsmaulHusna {
+import android.media.MediaPlayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ActivityAsmaulHusna extends AppCompatActivity {
+
+    RecyclerView recyclerView, recyclerView1;
+    RecyclerViewAdapter recyclerViewAdapter;
+    RecyclerViewAdapterBacaan recyclerViewAdapterBacaan;
+    RecyclerView.LayoutManager recyclerViewLayoutManager;
+    RecyclerView.LayoutManager recyclerViewLayoutManager1;
+    MediaPlayer mediaPlayer;
 }
