47d37ee8bd797794e17ffc8b13d1a1ca56f593aa
Musafak <musafak.ft18@mail.umy.ac.id>
2020-06-29 09:40:14 +0700

    Add class reclerview

--- app/src/main/java/com/example/asmaulhusna/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/asmaulhusna/RecyclerViewAdapter.java
@@ -1,4 +1,31 @@
 package com.example.asmaulhusna;
 
-public class RecyclerViewAdapter {
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecylerViewHolder> {
+    List<ActivityAsmaulHusna.User> values;
+    Context context1;
+
+
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.RecylerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.RecylerViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
