4876519863f61f7993f5f30f92062b04781609eb
Musafak <musafak.ft18@mail.umy.ac.id>
2020-06-29 09:48:26 +0700

    add class onBindViewholder

--- app/src/main/java/com/example/asmaulhusna/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/asmaulhusna/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.asmaulhusna;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,7 +30,23 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerViewAdapter.RecylerViewHolder holder, int position) {
+    public void onBindViewHolder( final RecylerViewHolder holder, final int position) {
+        holder.no.setText(values.get(position).getNo());
+        holder.ayat.setText(values.get(position).getAyat());
+        holder.bacaan.setText(values.get(position).getBacaan());
+        holder.arti.setText(values.get(position).getArti());
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(context1, ActivityAsmaulHusnaDetail.class);
+                myIntent.putExtra("position", position);
+                myIntent.putExtra("ayat", values.get(position).getAyat());
+                myIntent.putExtra("bacaan", values.get(position).getBacaan());
+                myIntent.putExtra("arti", values.get(position).getArti());
+                view.getContext().startActivity(myIntent);
+            }
+        });
 
     }
 
